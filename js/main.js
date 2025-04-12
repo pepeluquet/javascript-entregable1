df8f871ca1acd482d8ca007e792f1b0289432015
Christian Luquet <pepeluquet@gmail.com>
2025-04-12 11:37:04 -0300

    while

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// La fórmula para calcular el índice de masa corporal (IMC) es IMC = peso (kg) / estatura (m)*2
+
+// Interpretación de los resultados
+// El IMC es una manera estándar de determinar si una persona tiene un peso adecuado en relación a su estatura. Los rangos de IMC son: 
+// Bajo: menos de 18.5
+// Normal: 18.5 – 24.9
+// Sobrepeso: 25.0 – 29.9
+// Obesidad : mas de 30.0 
+
+const alumnos = [];
+
+console.log ("Bienvenido a carga de Alumnos")
+
+function imc(peso , altura){
+    return peso / ((altura/100) ** 2);
+};
+
+let continuar = true
+while (continuar) {
+    let nombre = prompt("Ingrese su nombre: ");
+    let apellido = prompt("Ingrese su apellido: ");
+    let edad = prompt("Ingrese su edad: ");
+    let peso = parseInt(prompt ("Ingrese su peso: "));
+    let altura = parseInt(prompt("Ingrese su altura (en centimetros): "));
+
+    imc(peso , altura);
+
+    let resultado = imc(peso, altura);
+
+    console.log (nombre)
+    console.log (apellido)
+    console.log (edad)
+    console.log (peso)
+    console.log (altura)
+    console.log (resultado)
+    console.log ("Tu Indice de Masa Corporal es: "+ resultado);
+
+    if (resultado < 18.5) {
+        console.log("Tienes un indice de masa corporal BAJO");
+    } else if (18.6 < resultado < 24.9) {
+        console.log("Tienes un indice de masa corporal NORMAL");
+    } else if (25 < resultado < 29.9) {
+        console.log("Tienes un indice de masa corporal SOBREPESO");
+    } else {
+        console.log("Tienes un indice de masa corporal OBESIDAD")
+    };
+    
+    alumnos.push = ({
+        nombre, apellido, edad, peso, altura, resultado,
+    });
+    
+
+    let confirmacion = prompt("Desea hacer otra carga? (si/no)")
+    if ( confirmacion == "no") {
+        continuar = false
+        console.log("Muchas gracias")
+    }
+}
+
+console.log(alumnos)
