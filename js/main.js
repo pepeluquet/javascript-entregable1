1a22951a124467f64572fa16ea0e175699eb331d
Christian Luquet <pepeluquet@hotmail.com>
2025-04-13 16:54:35 -0300

    trabajo sobre function y alerts

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
-// La fórmula para calcular el índice de masa corporal (IMC) es IMC = peso (kg) / estatura (m)*2
-
-// Interpretación de los resultados
-// El IMC es una manera estándar de determinar si una persona tiene un peso adecuado en relación a su estatura. Los rangos de IMC son: 
-// Bajo: menos de 18.5
-// Normal: 18.5 – 24.9
-// Sobrepeso: 25.0 – 29.9
-// Obesidad : mas de 30.0 
+console.log ("Bienvenido a carga de Alumnos")
 
 const alumnos = [];
 
-console.log ("Bienvenido a carga de Alumnos")
-
+// La fórmula para calcular el índice de masa corporal (IMC) es IMC = peso (kg) / estatura (m)*2
 function imc(peso , altura){
     return peso / ((altura/100) ** 2);
+};
+
+function saludo(nombre, apellido) {
+    alert("Bienvenido "+ nombre + " " + apellido);
 };
 
 let continuar = true
 while (continuar) {
     let nombre = prompt("Ingrese su nombre: ");
     let apellido = prompt("Ingrese su apellido: ");
-    let edad = prompt("Ingrese su edad: ");
+    saludo(nombre, apellido)
+    let edad = parseInt(prompt("Ingrese su edad: "));
     let peso = parseInt(prompt ("Ingrese su peso: "));
     let altura = parseInt(prompt("Ingrese su altura (en centimetros): "));
 
@@ -27,28 +24,32 @@ while (continuar) {
 
     let resultado = imc(peso, altura);
 
-    console.log (nombre)
-    console.log (apellido)
-    console.log (edad)
-    console.log (peso)
-    console.log (altura)
-    console.log (resultado)
     console.log ("Tu Indice de Masa Corporal es: "+ resultado);
+
+    // Interpretación de los resultados
+    // El IMC es una manera estándar de determinar si una persona tiene un peso adecuado en relación a su estatura. Los rangos de IMC son: 
+    // Bajo: menos de 18.5
+    // Normal: 18.5 – 24.9
+    // Sobrepeso: 25.0 – 29.9
+    // Obesidad : mas de 30.0 
 
     if (resultado < 18.5) {
         console.log("Tienes un indice de masa corporal BAJO");
+        alert("Tienes un indice de masa corporal BAJO");
     } else if (18.6 < resultado < 24.9) {
         console.log("Tienes un indice de masa corporal NORMAL");
+        alert("Tienes un indice de masa corporal NORMAL");
     } else if (25 < resultado < 29.9) {
         console.log("Tienes un indice de masa corporal SOBREPESO");
+        alert("Tienes un indice de masa corporal SOBREPESO");
     } else {
         console.log("Tienes un indice de masa corporal OBESIDAD")
+        alert("Tienes un indice de masa corporal OBESIDAD")
     };
     
     alumnos.push = ({
         nombre, apellido, edad, peso, altura, resultado,
     });
-    
 
     let confirmacion = prompt("Desea hacer otra carga? (si/no)")
     if ( confirmacion == "no") {
@@ -56,5 +57,4 @@ while (continuar) {
         console.log("Muchas gracias")
     }
 }
-
 console.log(alumnos)
